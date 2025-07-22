de8e17f7b9579e8aaa3de52271514a659d2b96ac
BigBoy <moshazly416@gmail.com>
2025-07-22 10:00:00 +0300

    create gift card product type

---
+++ GiftCard/Model/Product/Type/GiftCard.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+/**
+ * Virtual product type implementation
+ *
+ * @author     Magento Core Team <core@magentocommerce.com>
+ */
+
+namespace SwiftOtter\GiftCard\Model\Product\Type;
+
+class GiftCard extends \Magento\Catalog\Model\Product\Type\Virtual
+{
+    const string TYPE_CODE = 'giftcard';
+}
