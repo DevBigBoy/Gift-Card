7a53755909d614b55c0a9ddbfd9c7b589bab85e5
BigBoy <moshazly416@gmail.com>
2025-08-01 16:45:32 +0300

    create product attribute

---
+++ GiftCard/Attributes.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SwiftOtter\GiftCard;
+
+class Attributes
+{
+    const  string IS_CUSTOM_ALLOWED = 'is_custom_allowed';
+
+}
---
+++ GiftCard/Setup/Patch/Data/CreateISCustomAllowed.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace SwiftOtter\GiftCard\Setup\Patch\Data;
+
+use Magento\Catalog\Model\Product;
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use SwiftOtter\GiftCard\Attributes;
+
+/**
+* Patch is a mechanism that allows to do atomic upgrade data changes
+*/
+class CreateISCustomAllowed implements DataPatchInterface
+{
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+
+    /**
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+
+    /**
+     * ApplyAttributesUpdate constructor.
+     *
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param EavSetupFactory $eavSetupFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        \Magento\Eav\Setup\EavSetupFactory $eavSetupFactory
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+
+    /**
+     * @inheritdoc
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+     */
+    public function apply()
+    {
+        /** @var EavSetup $eavSetup */
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+
+        /**
+         * Add attributes to the eav/attribute
+         */
+        $eavSetup->addAttribute(
+            Product::ENTITY,
+            Attributes::IS_CUSTOM_ALLOWED,
+            [
+                'type' => 'int',
+                'backend' => '',
+                'frontend' => '',
+                'label' => 'Is Custom Amount Allowed',
+                'input' => 'select',
+                'class' => '',
+                'source' => \Magento\Eav\Model\Entity\Attribute\Source\Boolean::class,
+                'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_GLOBAL,
+                'visible' => true,
+                'required' => true,
+                'user_defined' => false,
+                'default' => '',
+                'searchable' => false,
+                'filterable' => false,
+                'comparable' => false,
+                'visible_on_front' => false,
+                'used_in_product_listing' => true,
+                'unique' => false,
+                'apply_to' => \SwiftOtter\GiftCard\Model\Product\Type\GiftCard::TYPE_CODE
+            ]
+        );
+
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+}
