125ec83bedd93b3368a1347def19c14b3f385eab
BigBoy <moshazly416@gmail.com>
2026-02-07 16:48:58 +0200

    update

--- GiftCard/Attributes.php
+++ GiftCard/Attributes.php
@@ -4,6 +4,5 @@ namespace SwiftOtter\GiftCard;
 
 class Attributes
 {
-    const  string IS_CUSTOM_ALLOWED = 'is_custom_allowed';
-
+    const string IS_CUSTOM_ALLOWED = 'is_custom_allowed';
 }
---
+++ GiftCard/ViewModel/FrontendDetail.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace SwiftOtter\GiftCard\ViewModel;
+
+use Magento\Framework\View\Element\Block\ArgumentInterface;
+
+class FrontendDetail implements ArgumentInterface
+{
+
+}
